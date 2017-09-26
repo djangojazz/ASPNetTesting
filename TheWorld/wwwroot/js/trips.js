96a66c534330ff0d4352f9f665d93347860e56d8
djangojazz <brettmorin2@gmail.com>
2017-09-26 07:23:29 -0700

    Made changes to code

---
+++ TheWorld/wwwroot/js/trips.js
@@ -0,0 +1,20 @@
+﻿// trips.js
+(function () {
+
+  "use strict";
+
+  // Creating the Module
+  angular.module("trips", ["simpleControls", "ngRoute"]);
+    .config(function ($routeProvider) {
+
+    $routeProvider.when("/", {
+      controller: "tripsController",
+      controllerAs: "vm",
+      templateUrl: "/views/tripsView.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/" });
+
+  });
+
+})();
